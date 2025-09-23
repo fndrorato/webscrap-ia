85c7150af0dc2dfd563c257f649ab0916593c653
Fernando Rorato <fernando.rorato@gmail.com>
2025-09-22 23:05:58 -0300

    novo logo

--- web/src/pages/AuthPages/AuthPageLayout.tsx
+++ web/src/pages/AuthPages/AuthPageLayout.tsx
@@ -28,7 +28,7 @@ export default function AuthLayout({
                 />
               </Link>
               <p className="text-center text-gray-400 dark:text-white/60">
-                {t("channelManagementSystem")}
+                Sistema de Búsqueda de Productos
               </p>
             </div>
           </div>
